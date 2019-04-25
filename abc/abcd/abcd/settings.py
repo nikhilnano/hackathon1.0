4b533b0aa020824f76cd569b851f338f2d2f3462
STARLORD3 <nileshsingh217@gmail.com>
2019-04-25 14:00:28 +0530

    Final commit

--- abc/abcd/abcd/settings.py
+++ abc/abcd/abcd/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat_app',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/hi/'
+CRISPY_TEMPLATE_PACK=  'bootstrap4'
--- abc/abcd/chat_app/admin.py
+++ abc/abcd/chat_app/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 # Register y1our models here.
 from .models import chat
+from .models import username
 admin.site.register(chat)
+admin.site.register(username)
